e2e303c61fef72dcd444b2949c68ec3f039afde4
Zein-Haddad <zeinhaddad02@gmail.com>
2020-02-12 09:45:15 +0700

    Added index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width = device-width, initial-scale = 1">
+	<title>Sistem Pelelangan Online</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+<!-- NavBar -->
+<div class="container-fluid">
+	<!--
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainNav" aria-expanded="false">
+					<span class="sr-only"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<h4 class="pull-left">Lelang Online</h4>
+			<div class="collapse navbar-collapse" id="mainNav">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">Pendataan Barang</a></li>
+					<li><a href="#">Penawaran</a></li>
+					<li><a href="#">Buka dan Tutup Lelang</a></li>
+					<li><a href="#">Generate Laporan</a></li>
+					<li class="btn-group" role="group">
+						<button class="btn btn-primary">Login</button>
+						<button class="btn btn-success">Register</button>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>-->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<p class="navbar-brand">Lelang Online</p>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="mainNav">
+			<ul class="nav navbar-nav mr-auto">
+				<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Pendataan Barang</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Penawaran</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Buka dan Tutup Lelang</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Generate Laporan</a></li>
+				<li class="nav-item">
+					<div class="btn-group">
+						<a class="btn btn-primary" href="#">Register</a>
+						<a class="btn btn-success" href="#">Login</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
