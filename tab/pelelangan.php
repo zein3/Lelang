5bda25baabb94ebdf8d8368ec4d37d4462194c4d
Zein-Haddad <zeinhaddad02@gmail.com>
2020-02-23 14:02:13 +0700

    Sedikit update

--- tab/pelelangan.php
+++ tab/pelelangan.php
@@ -23,8 +23,32 @@
 
 		while ($data = mysqli_fetch_array($query))
 		{
+			$id_barang = $data['id_barang'];
+			$query_barang = mysqli_query($connection, "Select * From tb_barang Where id_barang='$id_barang'");
+			$data_barang = mysqli_fetch_assoc($query_barang);
+
+			$id_user = null;
+			if ($data['id_user'] != null)
+			{
+				$id_user = $data['id_user'];
+			}
+
+
 			echo '<div class="card col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12">';
 			echo '<div class="card-body">';
+			echo '<h5 class="card-title">' . $data_barang['nama_barang'] . '</h5>';
+
+			$harga_rupiah = "Rp" . number_format($data_barang['harga_awal'],2,',','.');
+			echo '<h6 class="card-subtitle mb-2 text-muted">Harga awal: ' . $harga_rupiah . '</h6>';
+			if ($id_user != null)
+			{
+				$harga_akhir_rupiah = "Rp" . number_format($data['harga_akhir'],2,',','.');
+				echo '<h6 class="card-subtitle mb-2 text-muted">Penawaran tertinggi: ' . $harga_akhir_rupiah . '</h6>';
+			}
+
+			echo '<p class="card-text">' . $data_barang['deskripsi_barang'] . '</p>';
+
+			echo '<a class="btn btn-primary btn-block" href="#">Ajukan tawaran</a>';
 
 			echo '</div>';
 			echo '</div>';
